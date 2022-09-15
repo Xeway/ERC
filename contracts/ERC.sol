7d2c999e4d4597a6e4d26a01294885011c53254f
Ewan Humbert <xeway@protonmail.com>
2022-09-15 18:03:58 +0200

    Included return STABLE in getFullProperties

--- contracts/ERC.sol
+++ contracts/ERC.sol
@@ -364,6 +364,7 @@ contract ERC {
             address,
             address,
             OptionState,
+            address,
             address
         )
     {
@@ -378,8 +379,8 @@ contract ERC {
 
             for {
 
-            } lt(i, 0x180) {
-                // 0x180 == 384 == number of slots (= variables stored) * 32 bytes == 12 * 32
+            } lt(i, 0x1A0) {
+                // 0x1A0 == 416 == number of slots (= variables stored) * 32 bytes == 13 * 32
                 i := add(i, 0x20)
                 j := add(j, 0x01)
             } {
@@ -389,7 +390,7 @@ contract ERC {
                 ) // underlyingToken.slot == 0
             }
 
-            return(freeMemPointer, i) // i == 0x180 == add(add(freeMemPointer, i), 0x20)
+            return(freeMemPointer, i) // i == 0x1A0 == add(add(freeMemPointer, i), 0x20)
         }
 
         /* The assembly code above is the equivalent of :
@@ -406,7 +407,8 @@ contract ERC {
             writer,
             buyer,
             optionState,
-            address(WETH)
+            address(WETH),
+            address(STABLE)
         ); */
     }
 }
