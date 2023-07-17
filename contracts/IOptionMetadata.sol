2f05713a9539931f1e64e20fab60b8b20d3b2995
xeway <xeway@protonmail.com>
2023-07-17 19:16:27 +0200

    IOptionMetadata

---
+++ contracts/IOptionMetadata.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Option} from  "./Option.sol";
+
+interface IOptionMetadata {
+    function side() external view returns (Option.Side);
+
+    function underlyingToken() external view returns (address);
+
+    function amount() external view returns (uint256);
+
+    function quoteToken() external view returns (address);
+
+    function strike() external view returns (uint256);
+
+    function expiration() external view returns (uint256);
+
+    function durationExerciseAfterExpiration() external view returns (uint256);
+
+    function premiumToken() external view returns (address);
+
+    function premium() external view returns (uint256);
+
+    function getType() external view returns (Option.Type);
+
+    function writer() external view returns (address);
+
+    function buyer() external view returns (address);
+
+    function state() external view returns (Option.State);
+}
--- contracts/Option.sol
+++ contracts/Option.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract Option is Ownable {
     //                  //
