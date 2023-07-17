145345b0f4667e7caa380b7e37f56fc970da07a0
xeway <xeway@protonmail.com>
2023-07-17 19:24:19 +0200

    Added events in interface

--- contracts/IOption.sol
+++ contracts/IOption.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.0;
 
 interface IOption {
+    event Bought(address indexed buyer, uint256 timestamp);
+    event Exercised(uint256 timestamp);
+    event Expired(uint256 timestamp);
+    event Canceled(uint256 timestamp);
+
     function buy() external returns (bool);
-
     function exercise() external returns (bool);
-
     function retrieveExpiredTokens() external returns (bool);
-
     function cancel() external returns (bool);
 }
