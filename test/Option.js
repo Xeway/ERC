366029f52853d47bb11d8b4e948ba579d37c845f
mlalma <lassi.maksimainen@gmail.com>
2023-08-08 16:24:18 +0200

    Added unit tests for buy() call

--- test/Option.js
+++ test/Option.js
@@ -6,7 +6,7 @@ describe("Option", function () {
   const TOKEN2_START_BALANCE = 10 * 10 ** 6;
   const OPTION_COUNT = 1 * 10 ** 6;
   const STRIKE = 4 * 10 ** 5;
-  const PREMIUM = 2 * 10 ** 4;
+  const PREMIUM = 3 * 10 ** 4;
 
   async function deployInfraFixture() {
     const [owner, acct1, acct2, acct3] = await ethers.getSigners();
@@ -155,6 +155,197 @@ describe("Option", function () {
       await expect(optionContract.connect(acct1).create(callOption, [])).to.be.revertedWith(
         "ERC20: insufficient allowance"
       );
+    });
+  });
+
+  describe("Buying", function () {
+    it("Should buy call options", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 10;
+      const premiumPaid = (boughtOptions * PREMIUM) / OPTION_COUNT;
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.emit(optionContract, "Bought");
+
+      expect(await token1.balanceOf(optionContract.target)).to.equal(OPTION_COUNT);
+      expect(await token1.balanceOf(acct1.address)).to.equal(TOKEN1_START_BALANCE - OPTION_COUNT);
+      expect(await token1.balanceOf(acct2.address)).to.equal(TOKEN1_START_BALANCE);
+
+      expect(await token2.balanceOf(optionContract.target)).to.equal(0);
+      expect(await token2.balanceOf(acct1.address)).to.equal(TOKEN2_START_BALANCE + premiumPaid);
+      expect(await token2.balanceOf(acct2.address)).to.equal(TOKEN2_START_BALANCE - premiumPaid);
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(boughtOptions);
+    });
+
+    it("Should buy put options", async function () {
+      const { putOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+
+      const totalUnderlyingPrice = (OPTION_COUNT * STRIKE) / 10 ** 6;
+      await token2.connect(acct1).approve(optionContract.target, totalUnderlyingPrice);
+      await expect(optionContract.connect(acct1).create(putOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 10;
+      const premiumPaid = (boughtOptions * PREMIUM) / OPTION_COUNT;
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.emit(optionContract, "Bought");
+
+      expect(await token1.balanceOf(optionContract.target)).to.equal(0);
+      expect(await token1.balanceOf(acct1.address)).to.equal(TOKEN1_START_BALANCE);
+      expect(await token1.balanceOf(acct2.address)).to.equal(TOKEN1_START_BALANCE);
+
+      expect(await token2.balanceOf(optionContract.target)).to.equal(totalUnderlyingPrice);
+      expect(await token2.balanceOf(acct1.address)).to.equal(TOKEN2_START_BALANCE - totalUnderlyingPrice + premiumPaid);
+      expect(await token2.balanceOf(acct2.address)).to.equal(TOKEN2_START_BALANCE - premiumPaid);
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(boughtOptions);
+    });
+
+    it("Should fail to buy non-existent options", async function () {
+      const { optionContract, token2, acct2 } = await loadFixture(deployInfraFixture);
+      const boughtOptions = OPTION_COUNT / 10;
+      const premiumPaid = (boughtOptions * PREMIUM) / OPTION_COUNT;
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false))
+        .to.emit(optionContract, "Bought")
+        .to.be.revertedWith("state");
+    });
+
+    it("Should fail to buy options that are expired", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 10;
+      const premiumPaid = (boughtOptions * PREMIUM) / OPTION_COUNT;
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+
+      await time.increase(2 * 60 * 60);
+
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.be.rejectedWith(
+        "exceriseWindowEnd"
+      );
+    });
+
+    it("Should fail to buy options because buyer is not on the allowed buyer list", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2, acct3 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [acct3.address])).to.emit(
+        optionContract,
+        "Created"
+      );
+
+      const boughtOptions = OPTION_COUNT / 10;
+      const premiumPaid = (boughtOptions * PREMIUM) / OPTION_COUNT;
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.be.rejectedWith("allowedBuyers");
+
+      // acct3 should be able to buy, verify it!
+      await token2.connect(acct3).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct3).buy(0, OPTION_COUNT / 10, false)).to.emit(optionContract, "Bought");
+    });
+
+    it("Should fail to buy options because the amount of smaller than minimum buying lot size", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      const modifCallOption = {
+        ...callOption,
+        minBuyingLot: OPTION_COUNT / 5,
+      };
+
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(modifCallOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 5;
+      const premiumPaid = Math.ceil((boughtOptions * PREMIUM) / OPTION_COUNT);
+      await token2.connect(acct2).approve(optionContract.target, premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.be.rejectedWith("minBuyingLot");
+
+      // Check that we can buy when correct amount is defined
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 5, false)).to.emit(optionContract, "Bought");
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(boughtOptions);
+    });
+
+    it("Should fail to buy options because there are no options to be bought", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT;
+      const premiumPaid = Math.ceil((boughtOptions * PREMIUM) / OPTION_COUNT);
+      await token2.connect(acct2).approve(optionContract.target, 2 * premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT, false)).to.emit(optionContract, "Bought");
+
+      // All options bought, verify we can't buy any more of them
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT, false)).to.be.revertedWith("buyerOptionCount");
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(boughtOptions);
+    });
+
+    it("Should fail to buy options because we can't fill the whole amount", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 2;
+      const premiumPaid = Math.ceil((boughtOptions * PREMIUM) / OPTION_COUNT);
+      await token2.connect(acct2).approve(optionContract.target, 2 * premiumPaid);
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 2, false)).to.emit(optionContract, "Bought");
+
+      // We want to buy whole OPTION_COUNT slot, but can't since half is already gone
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT, true)).to.be.revertedWith("mustCompletelyFill");
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(boughtOptions);
+    });
+
+    it("Should fail to buy options because buyer has not approved token transfer", async function () {
+      const { callOption, optionContract, token1, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      await expect(optionContract.connect(acct2).buy(0, OPTION_COUNT / 10, false)).to.be.rejectedWith(
+        "ERC20: insufficient allowance"
+      );
+    });
+
+    it("Should fail due to the zero premium when buying options", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 2;
+      const premiumPaid = Math.ceil((boughtOptions * PREMIUM) / OPTION_COUNT);
+      await token2.connect(acct2).approve(optionContract.target, 2 * premiumPaid);
+      // Trying to buy too few option tokens, which would lead to zero premium
+      await expect(optionContract.connect(acct2).buy(0, 10, false)).to.be.revertedWith("premiumPaid");
+    });
+
+    it("Should properly adjust bought option count when modulo ≠ 0", async function () {
+      const { callOption, optionContract, token1, token2, acct1, acct2 } = await loadFixture(deployInfraFixture);
+      await token1.connect(acct1).approve(optionContract.target, OPTION_COUNT);
+      await expect(optionContract.connect(acct1).create(callOption, [])).to.emit(optionContract, "Created");
+
+      const boughtOptions = OPTION_COUNT / 2;
+      const premiumPaid = Math.ceil((boughtOptions * PREMIUM) / OPTION_COUNT);
+      await token2.connect(acct2).approve(optionContract.target, 2 * premiumPaid);
+
+      // Shouldn't be able to buy extra 17 tokens, instead the option token count should be OPTION_COUNT / 2
+      await expect(optionContract.connect(acct2).buy(0, 17 + OPTION_COUNT / 2, false)).to.emit(
+        optionContract,
+        "Bought"
+      );
+
+      expect(await token1.balanceOf(optionContract.target)).to.equal(OPTION_COUNT);
+      expect(await token1.balanceOf(acct1.address)).to.equal(TOKEN1_START_BALANCE - OPTION_COUNT);
+      expect(await token1.balanceOf(acct2.address)).to.equal(TOKEN1_START_BALANCE);
+
+      expect(await token2.balanceOf(optionContract.target)).to.equal(0);
+      expect(await token2.balanceOf(acct1.address)).to.equal(TOKEN2_START_BALANCE + premiumPaid);
+      expect(await token2.balanceOf(acct2.address)).to.equal(TOKEN2_START_BALANCE - premiumPaid);
+
+      expect(await optionContract.balanceOf(acct2.address, 0)).to.equal(OPTION_COUNT / 2);
     });
   });
 });
