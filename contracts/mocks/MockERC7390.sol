2ac4161d11668c9114678e304af447848d274d53
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2023-11-23 18:30:14 +0200

    Fixed the tests after ERC7390 contract is abstract and issuanceCounter is now private

---
+++ contracts/mocks/MockERC7390.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC7390 } from "../ERC7390.sol";
+
+contract MockERC7390 is ERC7390 {
+
+  /* solhint-disable-next-line no-empty-blocks */
+  constructor() ERC7390() {}
+}
